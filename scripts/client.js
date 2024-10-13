f79a7b6aca99455f36e04a9855d7e9503674e5e9
liammtech <liammtech@gmail.com>
2024-10-13 22:47:39 +0100

    Add winner client variable

--- scripts/client.js
+++ scripts/client.js
@@ -2,3 +2,11 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+const winnerClient = axios.create({
+    baseURL: process.env.WINNER_BASE_URL,
+    headers: {
+      'Authorization': `Bearer ${process.env.WINNER_API_KEY}`,
+      'Content-Type': 'application/json',
+    },
+  });
